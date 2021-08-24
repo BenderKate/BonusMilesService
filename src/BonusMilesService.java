e63fcaf8c03d315fdb5381f2cf37fcd6df4fe650
BenderKate <afonia96@gmail.com>
2021-08-24 18:29:11 +0300

    Correction

--- src/BonusMilesService.java
+++ src/BonusMilesService.java
@@ -1,6 +1,5 @@
 public class BonusMilesService {
     public int calculate(int cost) {
-        cost = 10_000;
         int mileCost = 20;
         int miles = cost / mileCost;
         return miles;
